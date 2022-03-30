9cdf2f73e370ac570502d2922c47164392e540aa
Henok <henok_206@yahoo.com>
2022-03-30 12:45:08 +0300

    pro init

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+<!-- 
+<footer class="section">
+  <div class="text-lg-center text-dark bg-light" >copyright @2022 Usermanegment</div>
+</footer> -->
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container-fluid">
+   
+
+  </div>
+</nav> -->
