c2f6efa52624fbe9de36446ffda2ddd30edad29d
Henok <henok_206@yahoo.com>
2022-03-30 12:45:34 +0300

    pro init

---
+++ template/heder.php
@@ -0,0 +1,58 @@
+
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+ <script src="js/bootstrap.bundle.min.js"></script>
+ <style>
+            #logincomponet{
+              border:0px solid red;
+                width:400px;
+                padding:10px;
+                margin: 20px auto;
+            }
+           #tablecontent{
+                border:0px solid red;
+                width:900px;
+                padding:10px;
+                margin: 10px auto;
+              }
+           button{
+             margin-left: 5px;
+           }
+           div.alert{
+                width:900px;
+                padding:10px;
+                margin: 10px auto;
+                text-align: center;
+                font-weight: 400;
+                
+           }
+           </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style="color: wheat;">User Management System</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page"   style="color: white"; href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style="color: white";  href="insert.php">newUser</a>
+        </li>
+      
+        <li class="nav-item">
+          <a class="nav-link"  style="color: white";   href="logout.php">logout</a>
+        </li>
+      </ul>
+   
+    </div>
+  </div>
+</nav>
+
+  
+
+  
