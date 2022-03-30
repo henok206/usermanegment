c6b6bb901f380fb1719659dee289a5b37aa8ef59
Henok <henok_206@yahoo.com>
2022-03-30 12:42:59 +0300

    pro init

--- index.php
+++ index.php
@@ -1,45 +1,23 @@
-<!-- start login componet -->
+
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
- <script src="js/bootstrap.bundle.min.js"></script>
- <style>
-            #logincomponet{
-              border:0px solid red;
-                width:400px;
-                padding:10px;
-                margin: 20px auto;
-            }
-           #tablecontent{
-                border:0px solid red;
-                width:900px;
-                padding:10px;
-                margin: 10px auto;
-              }
-           button{
-             margin-left: 5px;
-           }
-           </style>
-</head>
-<body>
-  <?php  include("heder/heder.php");?>
+
+  <?php  include('template/heder.php');?>
   <div id="logincomponet">
-    <form action="login.php" method="POST">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" name="username">
-        <!-- <div id="emailHelp" class="form-text">We'll never share your username with anyone else.</div> -->
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-  </div>
-        <!-- end of login componet -->
-      
-        
-</body>
-</html>
+   <form action="login.php" method="POST">
+     <div class="mb-3">
+       <label for="username" class="form-label">Username</label>
+       <input type="text" class="form-control" id="username" name="username">
+       <!-- <div id="emailHelp" class="form-text">We'll never share your username with anyone else.</div> -->
+     </div>
+     <div class="mb-3">
+       <label for="password" class="form-label">Password</label>
+       <input type="password" class="form-control" id="password" name="password">
+     </div>
+     <button type="submit" class="btn btn-primary">Login</button>
+   </form>
+ </div>
+  //<?php  include("template/footer.php");?>
+  </body>
+ </html>
